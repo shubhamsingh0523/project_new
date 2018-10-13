4bedb9483546f866575bfd75cdeea0c3ce6d5669
shubhamsingh0523 <44120076+shubhamsingh0523@users.noreply.github.com>
2018-10-13 22:55:29 +0530

    Add files via upload

---
+++ alphabet.py
@@ -0,0 +1,14 @@
+n=(input("enter character: "))
+m=n.lower()
+
+alphabet=["a","e","i","o","u","b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+
+if m in alphabet:
+     print("Alphabet")	 
+else:
+     print("NO")	 
+	 
+	 
+	 
+	 
+	 
