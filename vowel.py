1b8672f2c79d446e8025391d54f41a61db6b963e
shubhamsingh0523 <44120076+shubhamsingh0523@users.noreply.github.com>
2018-10-13 22:48:05 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,15 @@
+n=(input("enter charater: "))
+m=n.lower()
+vowel_list=["a","e","i","o","u"]
+non_vowel=["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+if m in vowel_list:
+     print("Vowel")
+elif m in non_vowel:
+     print("Consonent")	 
+else:
+     print("invalid")	 
+	 
+	 
+	 
+	 
+	 
